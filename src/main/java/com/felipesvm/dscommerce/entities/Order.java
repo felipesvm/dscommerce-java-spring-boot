0b8dc1c19b7381579536ab0934cc993eeb377747
felipesvm <federt49@gmail.com>
2026-01-04 19:42:12 -0300

    Entity Product, Category and Order Item Created

--- src/main/java/com/felipesvm/dscommerce/entities/Order.java
+++ src/main/java/com/felipesvm/dscommerce/entities/Order.java
@@ -2,7 +2,10 @@ package com.felipesvm.dscommerce.entities;
 
 import jakarta.persistence.*;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 
 @Entity
@@ -24,6 +27,8 @@ public class Order {
     @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
     private Payment payment;
 
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Order() {
     }
@@ -74,6 +79,14 @@ public class Order {
 
     public void setPayment(Payment payment) {
         this.payment = payment;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Product> getProducts() {
+        return items.stream().map(x -> x.getProduct()).toList();
     }
 
     @Override
