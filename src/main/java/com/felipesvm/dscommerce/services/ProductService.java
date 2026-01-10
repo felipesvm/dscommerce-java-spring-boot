64bacde14eb5d2678f32a28700e3edab1924976e
felipesvm <federt49@gmail.com>
2026-01-10 01:13:26 -0300

    ProductMinDTO, CategoryDTO, Ajuste ProductController, Ajuste ProductRepository

--- src/main/java/com/felipesvm/dscommerce/services/ProductService.java
+++ src/main/java/com/felipesvm/dscommerce/services/ProductService.java
@@ -8,7 +8,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.felipesvm.dscommerce.dto.CategoryDTO;
 import com.felipesvm.dscommerce.dto.ProductDTO;
+import com.felipesvm.dscommerce.dto.ProductMinDTO;
+import com.felipesvm.dscommerce.entities.Category;
 import com.felipesvm.dscommerce.entities.Product;
 import com.felipesvm.dscommerce.repositories.ProductRepository;
 import com.felipesvm.dscommerce.services.exceptions.DatabaseException;
@@ -30,9 +33,9 @@ public class ProductService {
     }
 
     @Transactional(readOnly = true)
-    public Page<ProductDTO> findAll(Pageable pageable) {
-        Page<Product> result = repository.findAll(pageable);
-        return result.map(x -> new ProductDTO(x));
+    public Page<ProductMinDTO> findAll(String name, Pageable pageable) {
+        Page<Product> result = repository.searchByName(name, pageable);
+        return result.map(x -> new ProductMinDTO(x));
     }
 
     @Transactional
@@ -74,6 +77,12 @@ public class ProductService {
         entity.setDescription(dto.getDescription());
         entity.setPrice(dto.getPrice());
         entity.setImgUrl(dto.getImgUrl());
+
+        entity.getCategories().clear();
+        for (CategoryDTO catDto : dto.getCategories()) {
+            Category cat = new Category();
+            cat.setId(catDto.getId());
+            entity.getCategories().add(cat);
+        }
     }
 }
-
